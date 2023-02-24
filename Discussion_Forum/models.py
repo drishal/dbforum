9c847b3d38c4ad6ef0e76171425badf93c04c55e
drishal <drishalballaney@gmail.com>
2023-02-24 10:50:55 +0530

    Initial commit

---
+++ Discussion_Forum/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# model for forum
+class forum(models.Model):
+    name=models.CharField(max_length=200,default="anonymous" )
+    email=models.CharField(max_length=200,null=True)
+    topic= models.CharField(max_length=300)
+    description = models.CharField(max_length=1000)
+    link = models.CharField(max_length=100, blank=True)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return str(self.topic)
+
+#model for discussion
+class Discussion(models.Model):
+    forum = models.ForeignKey(forum,blank=True,on_delete=models.CASCADE)
+    name=models.CharField(max_length=200,default="anonymous")
+    discuss = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return str(self.forum)
